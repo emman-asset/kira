4d3bc683542f797a9e933220b4e7da5f9a67e33d
emmannl <eul.nelson@gmail.com>
2020-01-26 20:30:08 +0100

    Initial commit

---
+++ sf1/assets/js/main.js
@@ -0,0 +1,51 @@
+
+// On small devices, remove the "open" class from the body element
+// as its presence causes issue with the  navigation menu an small
+// screens
+let removeOpenClass = () => {
+    let screenWidth = $(window).width();
+    if (screenWidth <= 577) {
+        $(`body`).removeClass('open');
+    }
+};
+
+$(document).ready(function($) {
+
+	"use strict";
+
+	[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {
+		new SelectFx(el);
+	} );
+
+	$('.selectpicker').selectpicker;
+
+
+	$('#menuToggle').on('click', function(event) {
+		$('body').toggleClass('open');
+	});
+
+	$('.search-trigger').on('click', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		$('.search-trigger').parent('.header-left').addClass('open');
+	});
+
+	$('.search-close').on('click', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		$('.search-trigger').parent('.header-left').removeClass('open');
+	});
+
+	// $('.user-area> a').on('click', function(event) {
+	// 	event.preventDefault();
+	// 	event.stopPropagation();
+	// 	$('.user-menu').parent().removeClass('open');
+	// 	$('.user-menu').parent().toggleClass('open');
+	// });
+
+
+    $(window).resize(function () {
+        removeOpenClass();
+    });
+    removeOpenClass();
+});
